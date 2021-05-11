aafe332bec265e9fd6082ece7c8dbfcfbae3e01a
Ilya Tokar <itokar98@gmail.com>
2021-05-11 12:50:44 +0300

    create parser

--- repl/repl.go
+++ repl/repl.go
@@ -3,10 +3,9 @@ package repl
 import (
 	"bufio"
 	"fmt"
-	"io"
-
 	"github.com/ilya-tokar/interpreter/lexer"
-	"github.com/ilya-tokar/interpreter/token"
+	"github.com/ilya-tokar/interpreter/parser"
+	"io"
 )
 
 const PROMPT = ">> "
@@ -23,9 +22,22 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, "Parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
